946b2164e03c42cbaf5491393f9053b87f4408be
Ryan Morrison <egodev@icloud.com>
2022-12-19 22:09:05 +0000

    Initial Commit

---
+++ CoffeeAndTV/CoffeeAndTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeAndTVApp.swift
+//  CoffeeAndTV
+//
+//  Created by Ryan Morrison on 19/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeAndTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
