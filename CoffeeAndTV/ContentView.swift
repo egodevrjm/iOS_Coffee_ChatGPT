c9f1db620d39519dd7ea06c3f2102c2b9547e0e9
Ryan Morrison <egodev@icloud.com>
2022-12-19 22:20:11 +0000

    First take

--- CoffeeAndTV/ContentView.swift
+++ CoffeeAndTV/ContentView.swift
@@ -8,19 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var coffeeList = CoffeeList()
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List(coffeeList.coffees) { coffee in
+                NavigationLink(destination: CoffeeDetail(coffee: coffee)) {
+                    Text(coffee.name)
+                }
+            }
+            .navigationBarTitle("Coffee Recipes")
+        }
+    }
+}
+
+struct CoffeeDetail: View {
+    var coffee: Coffee
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(coffee.name)
+                .font(.title)
+            Text("Category: \(coffee.category)")
+                .font(.subheadline)
+            Text("Ingredients:")
+                .font(.headline)
+            ForEach(coffee.ingredients, id: \.self) { ingredient in
+                Text("- \(ingredient)")
+            }
+            Text("Steps:")
+                .font(.headline)
+            ForEach(coffee.steps, id: \.self) { step in
+                Text("- \(step)")
+            }
         }
         .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+class CoffeeList: ObservableObject {
+    @Published var coffees: [Coffee] = []
+
+    init() {
+        let url = URL(string: "https://atripto.space/exp/coffee.json")!
+
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            do {
+                if let data = data {
+                    let coffeeData = try JSONDecoder().decode(CoffeeData.self, from: data)
+                    DispatchQueue.main.async {
+                        self.coffees = coffeeData.recipes
+                    }
+                } else {
+                    print("No data")
+                }
+            } catch {
+                print("Error: \(error)")
+            }
+        }.resume()
     }
+}
+
+struct Coffee: Codable, Identifiable {
+    var id: Int
+    var category: String
+    var name: String
+    var ingredients: [String]
+    var steps: [String]
+}
+
+struct CoffeeData: Codable {
+    var recipes: [Coffee]
 }
