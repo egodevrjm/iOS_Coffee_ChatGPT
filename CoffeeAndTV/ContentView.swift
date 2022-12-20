5f2785793a85fddd6974357b70bb74ecce7f951f
Ryan Morrison <upyourego@icloud.com>
2022-12-20 14:15:28 +0000

    Tweaked changes to the list and images

--- CoffeeAndTV/ContentView.swift
+++ CoffeeAndTV/ContentView.swift
@@ -8,13 +8,7 @@ struct ContentView: View {
         NavigationView {
             List(coffeeList.coffees) { coffee in
                 NavigationLink(destination: CoffeeDetail(coffee: coffee)) {
-                    HStack {
-                        Image("\(coffee.id)")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 50, height: 50)
-                        Text(coffee.name)
-                    }
+                    Text(coffee.name)
                 }
             }
             .navigationBarTitle("Coffee Recipes")
@@ -33,19 +27,12 @@ struct FavoritesView: View {
     var body: some View {
         List(coffeeList.favorites) { coffee in
             NavigationLink(destination: CoffeeDetail(coffee: coffee)) {
-                HStack {
-                    Image("\(coffee.id)")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 50, height: 50)
-                    Text(coffee.name)
-                }
+                Text(coffee.name)
             }
         }
         .navigationBarTitle("Favorites")
     }
 }
-
 
 struct CoffeeDetail: View {
     @ObservedObject var coffee: Coffee
